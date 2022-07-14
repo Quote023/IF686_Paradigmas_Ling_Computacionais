77e8f4cb3d239c97923d3e5af03eb9ce05e08e6f
quote023 <acs2@ecomp.poli.br>
2022-07-14 11:26:45 -0300

    add: semana 2 e 3

---
+++ haskell/Semana2.hs
@@ -0,0 +1,69 @@
+module Semana2 where
+
+square :: Int -> Int
+square x = x * x
+
+allEqual :: Int -> Int -> Int -> Bool
+allEqual n m p = (n == m) && (m == p)
+
+maxi :: Int -> Int -> Int
+maxi n m
+  | n >= m = m
+  | otherwise = m
+
+imc :: Int -> Int -> Int
+imc peso altura = div peso (square altura)
+
+verificarNumeros :: Int -> Int -> Int -> Bool
+verificarNumeros a b c = (a < b) && (b < c)
+
+ehPrimo :: Int -> Bool
+ehPrimo n
+  | n <= 2 = False
+  | even n = n == 2
+  | otherwise = ehPrimoI n 3
+  where
+    ehPrimoI n i
+      | mod n i == 0 = False
+      | square i > n = True
+      | otherwise = ehPrimoI n (i + 2)
+
+ehPrimoEntreSi :: Int -> Int -> Int
+ehPrimoEntreSi a 0 = a
+ehPrimoEntreSi a b = ehPrimoEntreSi b (rem b a)
+
+all4equal :: Int -> Int -> Int -> Int -> Bool
+all4equal a b c d = allEqual a b c && (c == d)
+
+equalCount :: Int -> Int -> Int -> Int
+equalCount a b c
+  | allEqual a b c = 3
+  | a == b || b == c || a == c = 2
+  | otherwise = 0
+
+vendas :: Int -> Int
+vendas n
+  | n == 0 = 12
+  | n == 1 = 24
+  | n == 2 = 30
+  | n == 3 = 30
+  | n == 4 = 30
+  | otherwise = 0
+
+totalVendas :: Int -> Int
+totalVendas 0 = vendas 0
+totalVendas n = totalVendas (n - 1) + vendas n
+
+maxVendas :: Int -> Int
+maxVendas 0 = vendas 0
+maxVendas n = maxi (maxVendas (n -1)) (vendas n)
+
+contarSemanasPorVendas :: Int -> Int -> Int
+contarSemanasPorVendas s n
+  | n == 0 = contar s 0
+  | otherwise = contarSemanasPorVendas s (n -1) + contar s n
+  where
+    contar s n = if vendas n == s then 1 else 0
+
+ehDigito :: Char -> Bool
+ehDigito ch = ('0' <= ch) && (ch <= '9')
---
+++ haskell/Semana3.hs
@@ -0,0 +1,87 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use map" #-}
+{-# HLINT ignore "Use foldr" #-}
+{-# HLINT ignore "Use list comprehension" #-}
+
+module Semana3 where
+
+import Semana2
+
+double :: [Int] -> [Int]
+double [] = []
+double (a : as) = (a * 2) : double as
+
+member :: [Int] -> Int -> Bool
+member [] e = False
+member (a : as) e = (a == e) || member as e
+
+digits :: String -> String
+digits [] = ""
+digits (c : cs)
+  | ehDigito c = c : digits cs
+  | otherwise = digits cs
+
+digits' :: String -> String
+digits' [] = ""
+digits' (c : cs) = (if ehDigito c then [c] else []) ++ digits cs
+
+sumPairs :: [Int] -> [Int] -> [Int]
+sumPairs [] [] = []
+sumPairs as [] = as
+sumPairs [] bs = bs
+sumPairs (a : as) (b : bs) = (a + b) : sumPairs as bs
+
+pegarEl :: [Int] -> Int -> Int
+pegarEl ar n = case pegarCaudaEm ar n of
+  [] -> 0
+  (a : as) -> a
+
+pegarCaudaEm :: [Int] -> Int -> [Int]
+pegarCaudaEm ar n = pegarCaudaEmI ar n 0
+  where
+    pegarCaudaEmI [] n i = []
+    pegarCaudaEmI (a : as) n i = if n == i then as else pegarCaudaEmI as n (i + 1)
+
+menoresQue :: [Int] -> Int -> [Int]
+menoresQue [] n = []
+menoresQue (a : as) n
+  | a < n = a : menoresQue as n
+  | otherwise = menoresQue as n
+
+maioresOuIguaisQue :: [Int] -> Int -> [Int]
+maioresOuIguaisQue [] n = []
+maioresOuIguaisQue (a : as) n
+  | a >= n = a : maioresOuIguaisQue as n
+  | otherwise = maioresOuIguaisQue as n
+
+quicksort :: [Int] -> [Int]
+quicksort [] = []
+quicksort (pivo : resto) =
+  quicksort (menoresQue resto pivo) ++ [pivo] ++ quicksort (maioresOuIguaisQue resto pivo)
+
+somarDigitos :: Int -> Int
+somarDigitos i = somarDigitosStr (show i)
+
+somarDigitosStr :: String -> Int
+somarDigitosStr [] = 0
+somarDigitosStr (a : as) = read [a] + somarDigitosStr as
+
+filtro :: [t] -> (t -> Bool) -> [t]
+filtro [] op = []
+filtro (a : as) op
+  | op a = a : filtro as op
+  | otherwise = filtro as op
+
+ordenarPelaSoma :: [Int] -> [Int]
+ordenarPelaSoma [] = []
+ordenarPelaSoma ar = pVal (qsTupla (montarPos ar))
+  where
+    montarPos [] = []
+    montarPos (b : bs) = (b, somarDigitos b) : montarPos bs
+    meq x (_, s) = s < x
+    maq x (_, s) = s >= x
+    qsTupla [] = []
+    qsTupla ((pv, ps) : r) = qsTupla (filtro r (meq ps)) ++ [(pv, ps)] ++ qsTupla (filtro r (maq ps))
+    pVal [] = []
+    pVal ((val, _) : r) = val : pVal r
